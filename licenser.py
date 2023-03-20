2ef9d36cee9ea732e9f464ac5472a3961368fcb5
nathanunghwan <nathanahn03@gmail.com>
2023-03-20 08:10:06 -0400

    onecase

--- licenser.py
+++ licenser.py
@@ -8,7 +8,7 @@ import time
 # Set the executable path and initialize Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
-time.sleep(60)
+
 # Visit the mars nasa news site
 #url = 'https://www.sircon.com/index.jsp/'
 url= 'https://www.sircon.com/ComplianceExpress/Inquiry/consumerInquiry.do?nonSscrb=Y'
@@ -35,48 +35,36 @@ for i in alpa_list:
             print(name_city)
             input_element = browser.find_by_name('lastName')
             input_element.fill(name_city)
-            time.sleep(10)
+            time.sleep(3)
             
             input_element = browser.find_by_name('city')
             input_element.fill(name_city)
-            time.sleep(10)
+            time.sleep(3)
             #license type
             license_element = browser.find_by_xpath('//*[@id="border"]/form[3]/div[6]/div[3]/select/option[21]').first
             license_element.click()
-            time.sleep(10)
+            time.sleep(3)
             #result 100
             element = browser.find_option_by_text('100').first.click()
             #//*[@id="resultsPerPage"]/select
-            time.sleep(10)
+            time.sleep(3)
             #submit
             submit_element=browser.find_by_id('submitButton').click()
             time.sleep(120)
+
+            
             # Parse the HTML
             html = browser.html
             table_soup = soup(html,'html.parser')
-            # even=table_soup.find_all(class_='ResultTableTextEvenRow')
-            # odd=table_soup.find_all(class_='ResultTableTextOddRow')
-
-            # for i in range(len(even)):
-            #     all_column=even[i].find_all('td', class_='ResultTableTextColumn')
-            #     name=all_column[0].text
-            #     license_num=all_column[1].text
-            #     npn=all_column[5].text
-            #     city=all_column[6].text
-            #     state=all_column[7].text
-            #     name_link = even[i].find('td', class_='ResultTableTextColumn').a.get('href')
-            #     print(name)
-        
-            elements = table_soup.select('tr[class*=ResultTableText]')    
+            data_table=table_soup.find('table', class_='DataTable')
             
-            for i in range(len(elements)):
-                all_column=elements[i].find_all('td', class_='ResultTableTextColumn')
-                name=all_column[0].text.replace('\n', '')
-                license_num=all_column[1].text.replace('\t', '').replace('\n', '').strip()
-                npn=all_column[5].text.replace('\n', '').strip()
-                city=all_column[6].text.replace('\n', '').strip()
-                state=all_column[7].text.replace('\n', '').strip()
-                name_link = elements[i].find('td', class_='ResultTableTextColumn').a.get('href')
+            if data_table is True:
+                name_one= data_table.find(class_='DataTableHeaderColumn').text
+                license_num=data_table.find_by_xpath('/html/body/table/tbody/tr[3]/td/form/table[4]/tbody/tr[2]/td[2]').text
+                npn=data_table.find_by_xpath('/html/body/table/tbody/tr[3]/td/form/table[2]/tbody/tr[2]/td').text
+                city=data_table.find_by_xpath('/html/body/table/tbody/tr[3]/td/form/table[3]/tbody/tr[1]/td[2]').text
+                state=data_table.find_by_xpath('/html/body/table/tbody/tr[3]/td/form/table[3]/tbody/tr[1]/td[2]').text
+                name_link = ""
                 abs_link=f'https://www.sircon.com{name_link}'
                 licenser_dic={}
                 licenser_dic["Name"] =name
@@ -89,6 +77,42 @@ for i in alpa_list:
             #         licenser_dic[key] = value.replace('\n', '')
 
                 licenser.append(licenser_dic)
+                state=table_soup.find_by_xpath('/html/body/table/tbody/tr[3]/td/form/div[4]/table[2]/tbody/tr[2]/td/input').click()
+            # even=table_soup.find_all(class_='ResultTableTextEvenRow')
+            # odd=table_soup.find_all(class_='ResultTableTextOddRow')
+            else:
+            # for i in range(len(even)):
+            #     all_column=even[i].find_all('td', class_='ResultTableTextColumn')
+            #     name=all_column[0].text
+            #     license_num=all_column[1].text
+            #     npn=all_column[5].text
+            #     city=all_column[6].text
+            #     state=all_column[7].text
+            #     name_link = even[i].find('td', class_='ResultTableTextColumn').a.get('href')
+            #     print(name)
+        
+                elements = table_soup.select('tr[class*=ResultTableText]')    
+                
+                for i in range(len(elements)):
+                    all_column=elements[i].find_all('td', class_='ResultTableTextColumn')
+                    name=all_column[0].text.replace('\n', '')
+                    license_num=all_column[1].text.replace('\t', '').replace('\n', '').strip()
+                    npn=all_column[5].text.replace('\n', '').strip()
+                    city=all_column[6].text.replace('\n', '').strip()
+                    state=all_column[7].text.replace('\n', '').strip()
+                    name_link = elements[i].find('td', class_='ResultTableTextColumn').a.get('href')
+                    abs_link=f'https://www.sircon.com{name_link}'
+                    licenser_dic={}
+                    licenser_dic["Name"] =name
+                    licenser_dic["License_number"] =license_num
+                    licenser_dic["NPN"] =npn
+                    licenser_dic["City"] =city
+                    licenser_dic["State"] =state
+                    licenser_dic["Link"] =abs_link
+                #     for key, value in licenser_dic.items():
+                #         licenser_dic[key] = value.replace('\n', '')
+
+                    licenser.append(licenser_dic)
         except TypeError:
         # Handle the case where item is None
             pass        
